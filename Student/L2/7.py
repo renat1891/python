4e705c94599fcef72ac6b979d0f23e8c3eb755b0
datsiuk7 <bogdandatcuk@gmail.com>
2025-10-10 19:57:25 +0300

    .

--- Student/L2/7.py
+++ Student/L2/7.py
@@ -9,12 +9,8 @@ class Rocket(Actor):
     def __init__(self, x, y):
         super().__init__("ship", (x, y))  
         self.speed = 5
-        self.lives = 3
-        self.score = 0
 
     def move(self):
-        # left, right, only up
-
         if keyboard.left:
             self.angle += 3
         if keyboard.right:
@@ -22,25 +18,8 @@ class Rocket(Actor):
         if keyboard.up:
             self.x += self.speed * -sin(radians(self.angle))
             self.y += self.speed * -cos(radians(self.angle))
+            
 
-        # if keyboard.left and self.left > 0:
-        #     self.x -= self.speed
-        # if keyboard.right and self.right < WIDTH:
-        #     self.x += self.speed
-        # if keyboard.up and self.top > 0:
-        #     self.y -= self.speed
-        # if keyboard.down and self.bottom < HEIGHT:
-        #     self.y += self.speed
-
-    def check_bottom(self):
-        if self.bottom >= HEIGHT:
-            self.lives -= 1
-            self.x = WIDTH // 2
-            self.y = HEIGHT // 2
-
-    def draw_hud(self):
-        screen.draw.text(f"Score: {self.score}", (20, 20), color="white", fontsize=36)
-        
 class Game():
     score = 0
     
@@ -51,7 +30,6 @@ class Game():
 class Coin(Actor):
     def __init__(self, x, y):
         super().__init__("coin", (x, y))
-        
 
     def random_move(self):
         self.x = random.randint(0, WIDTH)
@@ -60,32 +38,20 @@ class Coin(Actor):
 
 rocket = Rocket(WIDTH // 2, HEIGHT // 2)
 coin = Coin(100, 100)
-
+game = Game()
 
 def draw():
     screen.blit("bg", (0, 0))  
     rocket.draw()
     coin.draw()
-    Game().draw_points()
+    game.draw_points()
 
 def update():
-    
-
     rocket.move()
     
     if rocket.colliderect(coin):
         coin.random_move()
-        Game().score += 10
-    
-
-
-    
-    
-    
-
-    
-    
-
+        game.score += 10
 
 
 pgzrun.go()
