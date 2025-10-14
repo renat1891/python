f7659d62f9feb5236fc9dce95e18195d6dd0b17b
renat1891 <renatpochynok@gmail.com>
2025-10-14 16:21:36 +0300

    .

---
+++ Student/games/flappy_birds/1.py
@@ -0,0 +1,46 @@
+import pgzrun
+import random
+
+WIDTH = 800
+HEIGHT = 600
+
+class Game:
+    def __init__(self):
+        self.score = 0
+        self.lives = 3
+        self.game_over = False
+
+    def draw_points(self):
+        screen.draw.text(f"Score: {self.score}", (20, 20), color="white", fontsize=36)
+        screen.draw.text(f"Lives: {self.lives}", (20, 60), color="red", fontsize=36)
+        if self.game_over:
+            screen.draw.text("GAME OVER", center=(WIDTH//2, HEIGHT//2), color="yellow", fontsize=80)
+        
+class Bird(Actor):
+    def __init__(self, x, y):
+        super().__init__("bird", (x, y))
+        self.space_pressed = False
+
+    
+    def move(self):
+        if keyboard.space:
+            self.y -= 40
+            self.space_pressed = True
+        else:
+            self.space_pressed = False
+        self.y += 2
+        
+
+bird = Bird(100, 100)
+def draw():
+    screen.blit("background-day", (0, 0))
+    bird.draw()
+
+def update():
+    bird.move()
+
+pgzrun.go()
+    
+        
+        
+        
