42e54c8a1f8ede4a153528f3c2ad190a1604c07b
datsiuk7 <bogdandatcuk@gmail.com>
2025-12-12 19:16:29 +0200

    .

---
+++ Teacher/tgbot/1.py
@@ -0,0 +1,20 @@
+import asyncio # асинхронна біліотека 
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+bot = Bot(token="7756054942:AAHLVoZWAym72hYuebqTymYuPz8hgBkHd_U")
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    print(message.from_user)
+    await message.answer("Hello!")
+
+async def main():
+    print("Starting bot...")
+    await dp.start_polling(bot)
+    print("Bot stopped.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
