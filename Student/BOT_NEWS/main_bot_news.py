ee6999325eb588469d88899af8a515498f612963
renat1891 <renatpochynok@gmail.com>
2026-02-20 19:34:56 +0200

    .

--- Student/BOT_NEWS/main_bot_news.py
+++ Student/BOT_NEWS/main_bot_news.py
@@ -6,6 +6,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from parsing import get_data_info
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 
 TOKEN = "8462311437:AAEsjq9Yol5RFmOq0YGkVZlup1z5jsm4rFw"
 
@@ -15,41 +16,47 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 categories = [
-    "business", "entertainment", "general",
-    "health", "science", "sports",
-    "technology"
+    "main","eurointegration","epravda","life","mezha","oboronka","champion"
 ]
 
-
-# category_keyboard = InlineKeyboardMarkup(
-#     inline_keyboard=[
-#         [InlineKeyboardButton(text=cat.capitalize(), callback_data=cat)]
-#         for cat in categories
-#     ]
-# )
-
-
-# async def send_articles(chat_id, articles):
-#     if not articles:
-#         await bot.send_message(chat_id, "No articles found.")
-#         return
-
-#     for article in articles[:5]:
-#         text = (
-#             f"<b>{article['title']}</b>\n\n"
-#             f"{article.get('description', 'No description')}\n\n"
-#             f"{article['url']}"
-#         )
-#         await bot.send_message(chat_id, text, parse_mode="HTML")
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Усі Новини")]
+        [KeyboardButton(text="Головне"),
+            KeyboardButton(text="Політика")]
+        [KeyboardButton(text="Економіка"),
+            KeyboardButton(text="Життя")]
+        [KeyboardButton(text="Технології"),
+            KeyboardButton(text="Оборонка")]
+        [KeyboardButton(text="Спорт")]
+    ],
+    resize_keyboard=True
+)
 
 
-# @dp.message(Command("start"))
-# async def start_command(message: types.Message):
-#     subscribers.add(message.chat.id)
-#     await message.answer(
-#         "Welcome!\nSelect a news category:",
-#         reply_markup=category_keyboard
-#     )
+
+
+async def send_articles(chat_id, articles):
+    if not articles:
+        await bot.send_message(chat_id, "No articles found.")
+        return
+
+    for article in articles[:5]:
+        text = (
+            f"<b>{article['title']}</b>\n\n"
+            f"{article.get('description', 'No description')}\n\n"
+            f"{article['url']}"
+        )
+        await bot.send_message(chat_id, text, parse_mode="HTML")
+
+
+@dp.message(Command("start"))
+async def start_command(message: types.Message):
+    (message.chat.id)
+    await message.answer(
+        "Welcome!\nSelect a news category:",
+        reply_markup=keyboard
+    )
 
 
 async def main():
